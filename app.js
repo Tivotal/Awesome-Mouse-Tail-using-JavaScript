f4ed8e6f383b1602f7d34efef410eae3185229e7
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-11-07 07:16:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+/* Created by Tivotal */
+
+let canvas = document.querySelector(".canvas");
+let ctx = canvas.getContext("2d");
+let spots = [];
+let hue = 0;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+canvas.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 3; i++) {
+    spots.push(new tile());
+  }
+});
+
+class tile {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 2 + 0.1;
+    this.speedX = Math.random() * 2 - 1;
+    this.speedY = Math.random() * 2 - 1;
+    this.color = "hsl(" + hue + " 100%, 50%)";
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.1) this.size -= 0.03;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function handletile() {
+  for (let i = 0; i < spots.length; i++) {
+    spots[i].update();
+    spots[i].draw();
+    for (let j = i; j < spots.length; j++) {
+      let dx = spots[i].x - spots[j].x;
+      let dy = spots[i].y - spots[j].y;
+
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < 90) {
+        ctx.beginPath();
+        ctx.strokeStyle = spots[i].color;
+        ctx.lineWidth = spots[i].size / 10;
+        ctx.moveTo(spots[i].x, spots[i].y);
+        ctx.lineTo(spots[j].x, spots[j].y);
+        ctx.stroke();
+      }
+    }
+
+    if (spots[i].size <= 0.3) {
+      spots.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handletile();
+  hue++;
+  requestAnimationFrame(animate);
+}
+
+window.addEventListener("resize", function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+});
+
+window.addEventListener("mouseout", function () {
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
+
+animate();
